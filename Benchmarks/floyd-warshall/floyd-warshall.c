32680bfb27ef3c052aba3ad4616df97620efe7e9
tmccord26 <tmccord26@amherst.edu>
2025-05-01 21:05:53 -0400

    add floyd warshall c file

---
+++ Benchmarks/floyd-warshall/floyd-warshall.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#define V 5
+#define INF 100000000
+
+// Solves the all-pairs shortest path problem using Floyd-Warshall algorithm
+void floydWarshall(int dist[V][V]) {
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] != INF && dist[k][j] != INF &&
+                    dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    int dist[V][V] = {
+        {0, 4, INF, 5, INF},
+        {INF, 0, 1, INF, 6},
+        {2, INF, 0, 3, INF},
+        {INF, INF, 1, 0, 2},
+        {1, INF, INF, 4, 0}
+    };
+
+    floydWarshall(dist);
+
+    // Print the result
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("INF ");
+            else
+                printf("%d ", dist[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
